3894d13975b027f3644b2e03a0ab11be95b0c2b7
Basel-12 <bassilhamdi410@gmail.com>
2024-10-15 15:44:34 +0300

    basel last commit

--- obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3192087d9baf2b314f10b0aae7ab4a21b80c3154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5d9b29e6f1b97e74690980ed587c2f180ded64e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
